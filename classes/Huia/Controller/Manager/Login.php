73607fc6ad71a466a177551c65593b7faf4c90f4
Eduardo Pacheco <eduardo.pacheco@huia.com.br>
2015-04-29 14:50:06 -0300

    - Incluir login pela auth do AD da W3haus.

--- classes/Huia/Controller/Manager/Login.php
+++ classes/Huia/Controller/Manager/Login.php
@@ -2,15 +2,64 @@
 
 class Huia_Controller_Manager_Login extends Controller_Manager_App {
 
-	public $template = null;
+	public $template = NULL;
+	
+	protected function huia_auth($username, $password)
+	{
+		$auth_url = Kohana::$config->load('huia/manager.auth_url');
+		if ( ! $auth_url)
+		{
+			return FALSE;
+		}
+		
+		try
+		{
+			$request = Request::factory($auth_url);
+			$request->method(Request::POST);
+			$request->post('username', $username);
+			$request->post('password', $password);
+			$response = $request->execute();
+			
+			$user = @json_decode($response->body());
+			
+			if ( ! $user OR isset($user->error) OR ! isset($user->email))
+			{
+				return FALSE;
+			}
+			
+			$model = ORM::factory('User')->find_by_email($user->email);
+			$user->password = $password;
+			
+			if ( ! $model->loaded())
+			{
+				$model->values((array) $user);
+				$model->save();
+				$model->add('roles', array(1, 2));
+			}
+			else
+			{
+				$model->update();
+			}
+			
+			return Auth::instance()->login($username, $password);
+			
+		}
+		catch (Exception $e)
+		{
+			return FALSE;
+		}
+	}
 
 	public function action_index()
 	{
-		$post = $this->request->post();
-		if($this->request->method() == 'POST')
+		if ($this->request->method() === Request::POST)
 		{
-			$success = Auth::instance()->login($post['username'], $post['password']);		
-			if($success)
+			$username = $this->request->post('username');
+			$password = $this->request->post('password');
+			
+			$success = Auth::instance()->login($username, $password);
+			
+			if ($success OR $this->huia_auth($username, $password))
 			{
 				return HTTP::redirect('manager');
 			}
---
+++ config/huia/manager.php
@@ -0,0 +1,5 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array(
+	'auth_url' => 'http://sites.phphomolog.ncgroup.com.br/huia/auth/v1/',
+);
