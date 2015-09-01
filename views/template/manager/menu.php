9f67182bfae85eee3bc025d21a9429f565e2b3ef
Eduardo Pacheco <eduardo.pacheco@huia.com.br>
2015-09-01 04:31:50 -0300

    - Reconhecimento automato do menu.

--- views/template/manager/menu.php
+++ views/template/manager/menu.php
@@ -1,14 +1,55 @@
+<?php
+
+function render_menu($items, $parent = NULL)
+{ 
+  $html = '';
+  $ignored = array();
+  foreach ($items as $item)
+  {
+    if (in_array($item, $ignored))
+    {
+      continue;
+    }
+    
+    $matches = preg_grep('/^'.$item.'_/i', $items);
+    if ($matches AND ! $parent)
+    {
+      $html .= '<li><a href="#" class="dropdown-toggle" data-toggle="dropdown">';
+        $html .= __(Inflector::plural($item)) .' <span class="caret"></span>';
+      $html .= '</a>';
+      $html .= '<ul class="dropdown-menu">';
+        $html .= render_menu($matches, $item);
+      $html .= '</ul></li>';
+      
+      $ignored = array_merge($ignored, $matches);
+    }
+    else
+    {
+      $prepend = '';
+      if ($parent)
+      {
+        $underlines = count(explode('_', $item));
+        if ($underlines >= 3)
+        {
+          for ($i = $underlines; $i > 3; $i--)
+          {
+            $prepend .= '<i class="glyphicon glyphicon-option-horizontal"></i>';
+          }
+          $prepend .= '<i class="glyphicon glyphicon-triangle-right"></i>';
+        }
+      }
+      $html .= '<li><a href="./manager/'.strtolower($item).'">'.$prepend.__(Inflector::plural($item)).'</a></li>';
+    }
+  }
+  return $html;
+}
+
+?>
 <ul class="nav navbar-nav">
 	<?php
 	$model_classes = ORM_Autogen::get_models();
-	foreach ($model_classes as $model_class)
-	{
-		?>
-		<li>
-			<a href="./manager/<?php echo $model_class; ?>" target="_self" class="ng-binding"><?php echo __(Inflector::plural($model_class)); ?></a>
-		</li>
-		<?php
-	}
+  sort($model_classes);
+	echo render_menu($model_classes);
 	?>
 </ul>
 <ul class="nav navbar-nav navbar-right">
